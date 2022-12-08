b4a5e8a1c783404e6c03772595b5336d8d47f8bd
lchavan1 <lalu.chavan@optum.com>
2022-12-07 23:03:49 -0500

    first commit

--- src/main/java/arrays/TwoSum.java
+++ src/main/java/arrays/TwoSum.java
@@ -18,6 +18,22 @@ public class TwoSum {
     }
 
 
+    /*
+
+    Stores the previous difference with target as a key in the hashmap and then for every element checks if the needed difference is already in hashmap returns the index.
+
+
+     Initialize an empty HashMap.
+     Iterate over the elements of the array.
+     For every element in the array -
+     If the element exists in the Map, then check if it’s complement (target - element) also exists in the Map or not. If the complement exists then return the indices of the current element and the complement.
+     Otherwise, put the element in the Map, and move to the next iteration.
+
+
+     */
+
+
+
     private static  int[] findTwoSum(Integer[] values, int target)
     {
 
